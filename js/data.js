01bad32b34308b1efd0ac23c9ba224b57faf4c97
Yana <yanas@bluewin.ch>
2020-07-05 14:51:18 +0200

    Math.round

--- js/data.js
+++ js/data.js
@@ -9,8 +9,8 @@
     var pins = [];
 
     for (var i = 1; i <= pinsQuantity; i++) {
-      var locationX = window.util.getRandomArbitrary(0, ((document.querySelector('.map__pins').offsetWidth) - 100) - PIN_WIDTH / 2);
-      var locationY = window.util.getRandomArbitrary(130, 630) - PIN_HEIGHT;
+      var locationX = Math.round(window.util.getRandomArbitrary(0, ((document.querySelector('.map__pins').offsetWidth) - 100) - PIN_WIDTH / 2));
+      var locationY = Math.round(window.util.getRandomArbitrary(130, 630) - PIN_HEIGHT);
 
       var pin =
         {
@@ -21,10 +21,10 @@
           offer: {
             title: 'title',
             address: locationX + ', ' + locationY,
-            price: window.util.getRandomArbitrary(10000, 50000),
+            price: Math.round(window.util.getRandomArbitrary(10000, 50000)),
             type: window.util.getRandomElement(['palace', 'flat', 'house', 'bungalo']),
-            rooms: window.util.getRandomArbitrary(1, 5),
-            guests: window.util.getRandomArbitrary(1, 10),
+            rooms: Math.round(window.util.getRandomArbitrary(1, 5)),
+            guests: Math.round(window.util.getRandomArbitrary(1, 10)),
             checkin: window.util.getRandomElement(['12:00', '13:00', '14:00']),
             checkout: window.util.getRandomElement(['12:00', '13:00', '14:00']),
             features: window.util.getRandomSubset(['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']),
