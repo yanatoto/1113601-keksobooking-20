7e63950f3a279b5700a90fc011ade551c4555348
Yana <yanas@bluewin.ch>
2020-07-14 17:27:27 +0200

    Исправляет ошибки

--- js/map.js
+++ js/map.js
@@ -4,42 +4,42 @@
 (function () {
 
 
-    var onSuccessLoad = function (offer) {
-        window.pin.renderPins(offer);
+  var onSuccessLoad = function (offer) {
+    window.pin.renderPins(offer);
 
-    };
+  };
 
-    // активация страницы
+  // активация страницы
 
-    var mapFilters = document.querySelector('.map__filters');
-    var filtersFieldsets = mapFilters.querySelectorAll('fieldset');
-    var filtersSelect = mapFilters.querySelectorAll('select');
-    var map = document.querySelector('.map');
-
-
-    var activateMap = function () {
-        map.classList.remove('map--faded');
-        window.util.removeAttributeDisabled(filtersFieldsets);
-        window.util.removeAttributeDisabled(filtersSelect);
-        window.form.setAddress();
-        window.form.activateForm();
-        window.backend.load(onSuccessLoad, function () {});
-
-    };
-
-    var deactivateMap = function () {
-        map.classList.add('map--faded');
-        window.util.setAttributeDisabled(filtersFieldsets);
-        window.util.setAttributeDisabled(filtersSelect);
-        window.pin.removePins();
-        window.card.popupRemove();
-
-    };
-
-    window.map = {
-      activateMap: activateMap,
-      deactivateMap: deactivateMap
+  var mapFilters = document.querySelector('.map__filters');
+  var filtersFieldsets = mapFilters.querySelectorAll('fieldset');
+  var filtersSelect = mapFilters.querySelectorAll('select');
+  var map = document.querySelector('.map');
 
 
-    };
+  var activateMap = function () {
+    map.classList.remove('map--faded');
+    window.util.removeAttributeDisabled(filtersFieldsets);
+    window.util.removeAttributeDisabled(filtersSelect);
+    window.form.setAddress();
+    window.form.activateForm();
+    window.backend.load(onSuccessLoad, function () {});
+
+  };
+
+  var deactivateMap = function () {
+    map.classList.add('map--faded');
+    window.util.setAttributeDisabled(filtersFieldsets);
+    window.util.setAttributeDisabled(filtersSelect);
+    window.pin.removePins();
+    window.card.popupRemove();
+
+  };
+
+  window.map = {
+    activateMap: activateMap,
+    deactivateMap: deactivateMap
+
+
+  };
 })();
--- js/message.js
+++ js/message.js
@@ -1,52 +1,48 @@
 'use strict';
 
 (function () {
-    var successTemplate = document.querySelector('#success').content.querySelector('.success');
-    var errorTemplate = document.querySelector('#error').content.querySelector('.error');
-    var successText = successTemplate.cloneNode(true);
-    var errorText = errorTemplate.cloneNode(true);
-    var mainBlock = document.querySelector('main');
+  var successTemplate = document.querySelector('#success').content.querySelector('.success');
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var successText = successTemplate.cloneNode(true);
+  var errorText = errorTemplate.cloneNode(true);
+  var mainBlock = document.querySelector('main');
 
 
-    var showMessage = function (result) {
-        switch (result) {
-        case 'success':
-            document.body.appendChild(successText);
+  var showMessage = function (result) {
+    switch (result) {
+      case 'success':
+        document.body.appendChild(successText);
 
-            break;
-        case 'error':
-            mainBlock.appendChild(errorText);
+        break;
+      case 'error':
+        mainBlock.appendChild(errorText);
 
-            break;
-        }
-        document.addEventListener('click', closeMessage);
-        document.addEventListener('keydown', closeMessage);
-        //var errorButton = document.querySelector('.error__button');
-        //errorButton.addEventListener('click', closeMessage);
+        break;
+    }
+    document.addEventListener('click', closeMessage);
+    document.addEventListener('keydown', closeMessage);
+    // var errorButton = document.querySelector('.error__button');
+    // errorButton.addEventListener('click', closeMessage);
 
-    };
-    var setsuccessText = function (successText) {
-      successText.value = successText;
+  };
 
-    };
+  var closeMessage = function (evt) {
+    if (successText && evt.key === 'Escape'|| evt.button === 0) {
+      successText.remove();
+      window.main.deactivatePage();
 
-    var closeMessage = function (evt) {
-        if (successText && evt.key === 'Escape'|| evt.button === 0) {
-            successText.remove();
-            window.main.deactivatePage();
-
-        } if (errorText && evt.key === 'Escape' || evt.button === 0) {
-            errorText.remove();
-            window.main.deactivatePage();
-        }
-        document.removeEventListener('click', closeMessage);
-        document.removeEventListener('keydown', closeMessage);
-    };
+    } if (errorText && evt.key === 'Escape' || evt.button === 0) {
+      errorText.remove();
+      window.main.deactivatePage();
+    }
+    document.removeEventListener('click', closeMessage);
+    document.removeEventListener('keydown', closeMessage);
+  };
 
 
-    window.message = {
-        closeMessage: closeMessage,
-        showMessage: showMessage
+  window.message = {
+    closeMessage: closeMessage,
+    showMessage: showMessage
 
-    };
+  };
 })();
